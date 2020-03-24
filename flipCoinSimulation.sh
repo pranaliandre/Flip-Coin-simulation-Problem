7067537af3795a700b27ab1ba6ba09dc2f2597cd
pranaliandre <pranaliandre06@gmail.com>
2020-03-24 06:41:47 -0500

    Added: Simulator start with flipping a coin to display Heads and Tails

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,1 +1,16 @@
+#!/bin/bash -x
 echo "Welcome in flip coin simulation"
+#function to flip coin
+function flipCoin(){
+   local randomFlip=$((RANDOM%2))
+   if [ $randomFlip -eq 1 ] ;
+   then
+      echo Heads
+   else
+      echo Tails
+   fi
+}
+#one coin simulation
+flipCoin
+
+
